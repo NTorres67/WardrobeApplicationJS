32a68430474bf9cf47d421875f9d28b76281d2a9
NTorres67 <ntorres67@sbcglobal.net>
2018-03-24 11:29:00 -0400

    Added: navbar btn to Hilo Hatties; Alert: directed to another site; Confirm/Cancels opening new tab to website

--- WardrobeApplication/WardrobeScript.js
+++ WardrobeApplication/WardrobeScript.js
@@ -26,16 +26,17 @@ window.onclick = function (event) {
 }
 
 
-function letsShop() {
-    var result = link("https://www.hilohattie.com/");
+function overBtn(x) {
+    x.style.backgroundColor = "white";
+    x.style.color = "00ccff";
 }
 
+function outBtn(x) {
+    x.style.backgroundColor = "#474e5d";
+    x.style.color = "white";
+}
 
-
-
-
-
-function Redirect(x) {
+function letsShop(x) {
     var confirmButton = document.getElementById(x);
     var userResponse = confirm('You are about to leave this site. If you want to stay, please select cancel.');
     var displayContainer = document.getElementById('confirmResponse');
